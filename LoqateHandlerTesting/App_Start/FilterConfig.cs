94bc080726105c2f1f1e5331ed5f4511d05772b5
Austin Nolz <nolzaj93@gmail.com>
2022-05-26 16:10:54 -0400

    Add project files.

---
+++ LoqateHandlerTesting/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LoqateHandlerTesting
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
